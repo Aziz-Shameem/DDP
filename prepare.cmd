6aac37ebb4f3be8abef50c6ae72c4b20934eec8d
Aziz Shameem <81754147+Aziz-Shameem@users.noreply.github.com>
2025-06-20 10:20:26 +0530

    Delete prepare.cmd

--- prepare.cmd
+++
@@ -1,8 +0,0 @@
-pip install wavemix
-pip install torch-fidelity
-pip install torchmetrics[image]
-pip install datasets
-git clone https://github.com/KeKsBoTer/torch-dwt
-cd torch-dwt
-pip install -e .
-cd ..
